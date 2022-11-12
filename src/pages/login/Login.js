ce32ab057980e765d9435e819dc7e516139a9c62
Himanshu66a <82566349+Himanshu66a@users.noreply.github.com>
2022-11-12 16:15:21 +0530

    Update Login.js

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -27,9 +27,7 @@ export default function Login() {
       { email: email.current.value, password: password.current.value },
       dispatch
     );
-    await axios.get(`${AU}api/auth/login/${email.current.value}/${password.current.value}`).then(res => {
-      localStorage.setItem("user", JSON.stringify(res))
-    })
+    await axios.get(`${AU}api/auth/login/${email.current.value}/${password.current.value}`)
     .catch((error) => {
       if (error.response) {
         if (error.response.status == 404) {
